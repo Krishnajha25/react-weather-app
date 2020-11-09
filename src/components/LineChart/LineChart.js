9251a8472bc10dc925ebb6e50daa85d9a2aea9e6
Krishnajha25 <krishnajha366@gmail.com>
2020-11-10 00:05:23 +0530

    New changes

--- src/components/LineChart/LineChart.js
+++ src/components/LineChart/LineChart.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import {
-  AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip,
+  AreaChart, Area, XAxis, CartesianGrid, Tooltip,
 } from 'recharts';
 import './LineChart.css';
 
@@ -59,18 +59,14 @@ export default class LineChartView extends PureComponent {
         margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
         <defs>
           <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-            <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-          </linearGradient>
-          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
-            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+            <stop offset="5%" stopColor="#86d4fc" stopOpacity={0.8}/>
+            <stop offset="65%" stopColor="#86d4fc" stopOpacity={0}/>
           </linearGradient>
         </defs>
         <XAxis dataKey="name" />
         <CartesianGrid strokeDasharray="3 3" />
         <Tooltip />
-        <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+        <Area type="monotone" dataKey="uv" stroke="#00a6fa" fillOpacity={1} fill="url(#colorUv)" />
       </AreaChart>
     );
   }
