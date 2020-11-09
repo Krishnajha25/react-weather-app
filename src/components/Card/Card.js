f307e1cd8bfeee0faaf8332b5f5f6b870781f206
Krishnajha25 <krishnajha366@gmail.com>
2020-11-09 23:30:26 +0530

    New changes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './Card.css'
 import Temperature from '../Temperature/Temperature'
-import Graph1 from '../Graph1/Graph1'
-import Graph2 from '../Graph2/Graph2'
+// import Graph1 from '../Graph1/Graph1'
+// import Graph2 from '../Graph2/Graph2'
+import LineChart from '../LineChart/LineChart'
 
 function Card() {
     return (
         <div className="card_container">
             <Temperature />
-            <Graph1 />
-            <Graph2 />
+            
+            <div className="lineChartContainer">
+                <LineChart />
+            </div>
+            {/* <Graph1 />
+            <Graph2 /> */}
         </div>
     )
 }
